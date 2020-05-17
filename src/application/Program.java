6b198940c65b18f02e98e1d2855a7b5414c82f16
Pedro Braga <bragavpedro@hotmail.com>
2020-05-17 13:44:31 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,7 +5,9 @@
  */
 package application;
 
+import java.util.Date;
 import model.entities.Department;
+import model.entities.Seller;
 
 /**
  *
@@ -15,8 +17,15 @@ public class Program {
     
     
     public static void main(String[] args){
+        
+        
         Department obj = new Department(1, "Books");
-        System.out.println(obj);
+        
+        Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+        
+        System.out.println(seller);
+        
+        
     }
     
 }
