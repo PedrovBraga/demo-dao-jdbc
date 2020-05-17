455290da85c5e8f80108df3cfc6f75afd71a892e
Pedro Braga <bragavpedro@hotmail.com>
2020-05-17 13:02:53 -0300

    Department entity class

---
+++ src/application/Program.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package application;
+
+import model.entities.Department;
+
+/**
+ *
+ * @author julia
+ */
+public class Program {
+    
+    
+    public static void main(String[] args){
+        Department obj = new Department(1, "Books");
+        System.out.println(obj);
+    }
+    
+}
